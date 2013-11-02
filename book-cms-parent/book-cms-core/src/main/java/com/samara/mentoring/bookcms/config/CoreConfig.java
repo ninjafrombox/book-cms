6f422cec8df25597e5e08b24829636313066ca87
ninjafrombox <ninjafrombox@users.noreply.github.com>
2013-11-02 23:45:11 +0300

    [git] next cosmetic commit

--- book-cms-parent/book-cms-core/src/main/java/com/samara/mentoring/bookcms/config/CoreConfig.java
+++ book-cms-parent/book-cms-core/src/main/java/com/samara/mentoring/bookcms/config/CoreConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 /**
  * Spring beans configuration
  * Cosmetic commit
- * Cosmetic commit for next release
  */
 @Configuration
 public class CoreConfig {
