df5dcb54df759025c85030987ba4b9fc4475aaeb
ninjafrombox <ninjafrombox@users.noreply.github.com>
2013-12-05 21:31:44 +0400

    [JBoss] security

--- book-cms-parent/book-cms-web/src/main/java/com/samara/mentoring/bookcms/servlet/IndexServlet.java
+++ book-cms-parent/book-cms-web/src/main/java/com/samara/mentoring/bookcms/servlet/IndexServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 @WebServlet("/")
+@ServletSecurity(value = @HttpConstraint(rolesAllowed = "ldapUser"))
 public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
